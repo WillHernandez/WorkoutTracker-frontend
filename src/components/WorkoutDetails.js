b325463cb5142250aebac6f9c8d119e3b440443e
Will Hernandez <w.hernandez.code@gmail.com>
2022-08-03 15:07:52 -0400

    Initial frontend commit

---
+++ src/components/WorkoutDetails.js
@@ -0,0 +1,36 @@
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+import { format } from 'date-fns';
+
+const WorkoutDetails = ({workout}) => {
+	const { _id, title, load, reps, createdAt} = workout;
+	const { dispatch } = useWorkoutsContext();
+
+	const handleClick = async (e) => {
+		try {
+			e.preventDefault();
+			const response = await fetch(`/api/workouts/${_id}`, {
+				method: "DELETE"
+			})
+			if(response.ok) {
+				const json = await response.json();
+				dispatch({type: 'DELETE_WORKOUT', payload: json});
+			}
+		} catch (err) {
+			console.error(err);
+		}
+
+
+	}
+
+	return(
+		<div className="workout-details">
+			<h4>{title}</h4>
+			<p><strong>Load (lb): </strong>{load}</p>
+			<p><strong>Reps: </strong>{reps}</p>
+			<p>{format(new Date(createdAt), 'PPpp')}</p>
+			<span onClick={handleClick} className="material-symbols-outlined">delete</span>
+		</div>
+	)
+}
+
+export default WorkoutDetails;
---
+++ src/components/WorkoutForm.js
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+
+const WorkoutForm = () => {
+	const { dispatch } = useWorkoutsContext();
+	const [title, setTitle] = useState("");
+	const [load, setLoad] = useState(0);
+	const [reps, setReps] = useState("");
+	const [error, setError] = useState(null);
+	const [emptyFields, setEmptyFields] = useState([]);
+
+	const handleSubmit = async (e) => {
+		try {
+			e.preventDefault();
+			const workout = { title, load, reps };
+			const response = await fetch("/api/workouts/", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(workout)
+			})
+			const json = await response.json();
+			
+			if(response.ok) {
+				setTitle("");
+				setLoad(0);
+				setReps("");
+				setError(null);
+				setEmptyFields([]);
+				console.log('new workout added', json);
+				dispatch({type: "CREATE_WORKOUT", payload: json});
+			} else {
+				setError(json.err);
+				setEmptyFields(json.emptyFields);
+			}
+		} catch(e) {
+			setError(e);
+		}
+	}
+
+	return(
+		<form className="create" onSubmit={handleSubmit}>
+			<h3>Add a new workout</h3>
+
+			<label>Excercise Title</label>
+			<input 
+				type="text" 
+				onChange={e => setTitle(e.target.value)} 
+				value={title} 
+				className={emptyFields.includes('title') ? "error" : ""}
+			/>
+
+			<label>Load (in LB)</label>
+			<input 
+				type="number" 
+				onChange={e => setLoad(e.target.value)} 
+				value={load} 
+				placeholder="0"
+			/>
+			
+			<label>Reps</label>
+			<input 
+				type="number" 
+				onChange={e => setReps(e.target.value)} 
+				value={reps} 
+				placeholder="0"
+				className={emptyFields.includes('reps') ? "error" : ""}
+			/>
+
+			<button>Add Workout</button>
+			{error && <div className="error">{error}</div>}
+		</form>
+	)
+}
+
+export default WorkoutForm;
